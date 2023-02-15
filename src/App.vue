582712378ba2009f93d1cb1fb17f1d139f604ba7
zuoxunwang <187129090@qq.com>
2023-02-15 15:44:48 +0800

    commit all

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ count }}
+    <br>
+    {{ doubleCount }}
+    <button @click="$store.commit('increament')">count++</button>
+    <button @click="$store.dispatch('increament')">count++</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+computed:{
+  count(){
+    return this.$store.state.count
+  },
+  doubleCount(){
+    return this.$store.getters.doubleCount
   }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import VouRouter from 'vue-router'
 
+Vue.use(Vuex)
+Vue.use(VouRouter)
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state:{
+    count:0
+  },
+  mutations:{
+    increament(state){
+      state.count++
+    }
+  },
+  actions:{
+    increament({commit}){
+      setTimeout(() => {
+        commit('increament')
+      }, 2000);
+    }
+  },
+  getters:{
+    doubleCount(state){
+      return state.count * 2
+    }
+  }
+})
+
+const router = new VouRouter({
+  // mode:history,
+  routes,
+})
 new Vue({
+  store,routes,
   render: h => h(App),
 }).$mount('#app')
